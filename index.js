18b9741d1d1762abb2cf89b59e51026cd9beaeea
aliarnoos <alimarnoos@gmail.com>
2022-07-15 14:49:16 +0300

    finished the project with delete btn and change read btn

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ function closePopUp () {
 
 }
 
-const ali= new Book('football', 'ali', 200, true)
+const newBook = new Book ('Book', 'someone', 1000, 'Read')
+
 const card = document.createElement('div')
 const liT = document.createElement('h3')
 const liA = document.createElement('h3')
@@ -38,16 +39,42 @@ const liP = document.createElement('h3')
 const liR = document.createElement('h3')
 card.setAttribute('class', 'card')
 
-liT.innerText= 'Title: ' + ali.title;
-liA.innerText= 'Author: ' + ali.author;
-liP.innerText= 'Pages: ' + ali.pages;
-liR.innerText= 'Read Status: ' + ali.read;
+liT.innerText= 'Title: ' + newBook.title;
+liA.innerText= 'Author: ' + newBook.author;
+liP.innerText= 'Pages: ' + newBook.pages;
+liR.innerText= 'Read Status: ' + newBook.read;
 
-cards.appendChild(card)
+cards.appendChild(card);
 card.appendChild(liT)
 card.appendChild(liA)
 card.appendChild(liP)
 card.appendChild(liR)
+
+const btns = document.createElement('div')
+btns.classList.add('change-btns')
+card.appendChild(btns)
+const readBtn = document.createElement('button')
+readBtn.innerText="change"
+btns.appendChild(readBtn)
+readBtn.addEventListener('click', function changeRead () {
+  if(newBook.read=="Read") {
+    newBook.read="Not Read"
+  }
+  else if (newBook.read=="Not Read") {
+    newBook.read="Read"
+  }
+  liR.innerText= 'Read Status: ' + newBook.read;
+})
+
+const delBtn = document.createElement('button')
+delBtn.innerText="Remove Book"
+delBtn.classList.add('del-btn')
+btns.appendChild(delBtn)
+
+delBtn.addEventListener('click', function removeElement() {
+  cards.removeChild(card);
+})
+
 
 add.addEventListener('click', function addObject () {
   if (title.value!="" && author.value!="" && pages.value!="") {
@@ -60,16 +87,44 @@ add.addEventListener('click', function addObject () {
     const liR = document.createElement('h3')
     card.setAttribute('class', 'card')
     
-    liT.innerText= 'Title: ' + newBook.title;
-    liA.innerText= 'Author: ' + newBook.author;
-    liP.innerText= 'Pages: ' + newBook.pages;
-    liR.innerText= 'Read Status: ' + newBook.read;
-    
     cards.appendChild(card);
     card.appendChild(liT)
     card.appendChild(liA)
     card.appendChild(liP)
     card.appendChild(liR)
+
+    const btns = document.createElement('div')
+    btns.classList.add('change-btns')
+    card.appendChild(btns)
+    const readBtn = document.createElement('button')
+    readBtn.innerText="change"
+    btns.appendChild(readBtn)
+    readBtn.addEventListener('click', function changeRead () {
+      if(newBook.read=="Read") {
+        newBook.read="Not Read"
+      }
+      else if (newBook.read=="Not Read") {
+        newBook.read="Read"
+      }
+      liR.innerText= 'Read Status: ' + newBook.read;
+    })
+
+
+    liT.innerText= 'Title: ' + newBook.title;
+    liA.innerText= 'Author: ' + newBook.author;
+    liP.innerText= 'Pages: ' + newBook.pages;
+    liR.innerText= 'Read Status: ' + newBook.read;
+
+    
+
+    const delBtn = document.createElement('button')
+    delBtn.innerText="Remove Book"
+    delBtn.classList.add('del-btn')
+    btns.appendChild(delBtn) 
+
+    delBtn.addEventListener('click', function removeElement() {
+      cards.removeChild(card);
+   })
 
     title.value=null;
     author.value=null;
